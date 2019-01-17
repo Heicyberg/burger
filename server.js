2dc15b6c9971362fc31db3f22224cd22784e1741
Bingxi Du <bxdu@Bingxis-MacBook-Pro.local>
2019-01-16 23:24:55 -0500

    using path

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var path = require("path")
 
 var PORT = process.env.PORT || 8080;
 
@@ -18,7 +19,8 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgers_Controller.js");
+var routes = require(path.join(__dirname, "/controllers/burgers_Controller.js"));
+
 
 app.use(routes);
 
